63d7e1f33c670c43d77c0b6a700788a0a6b4f32f
Hardonian <118695431+Hardonian@users.noreply.github.com>
2026-06-13 00:53:07 +0000

    🧪 Add tests for webhook alerter

---
+++ reply.py
@@ -0,0 +1,9 @@
+import json
+
+replies = [
+    {
+        "comment_id": "4696821196",
+        "reply": "Understood. Acknowledging that this PR is a duplicate and stopping work on this task."
+    }
+]
+print(json.dumps(replies))
