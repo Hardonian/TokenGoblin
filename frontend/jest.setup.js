0f1273cb7188c07af7828c388664bf81b9f24f13
Hardonian <118695431+Hardonian@users.noreply.github.com>
2026-07-24 07:20:13 +0000

    🧪 [testing improvement] Add tests for Tenant Register API

--- frontend/jest.setup.js
+++ frontend/jest.setup.js
@@ -1,2 +1,22 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
 require('@testing-library/jest-dom');
+
+// Mock IntersectionObserver for framer-motion in jsdom
+class IntersectionObserver {
+  constructor(callback, options) {
+    this.callback = callback;
+    this.options = options;
+  }
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+global.IntersectionObserver = IntersectionObserver;
+
+// Mock ResizeObserver
+class ResizeObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+global.ResizeObserver = ResizeObserver;
